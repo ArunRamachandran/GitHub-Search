84127833e4fe4d172f77ea06dd7accf4576cf8bb
ArunRamachandran <arunkramachandran92@gmail.com>
2017-07-06 08:14:16 +0530

    Updated repository open issues list view

--- app/components/RepositoryOpenIssues.jsx
+++ app/components/RepositoryOpenIssues.jsx
@@ -2,6 +2,7 @@
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import '../stylesheets/repositoryOpenIssues.scss';
 
 export default class RepositoryOpenIssues extends Component {
 
@@ -9,11 +10,23 @@ export default class RepositoryOpenIssues extends Component {
 		super(props);
 	}
 
+	getDateDifference = (created_at) => {
+		let today = new Date();
+		let createdDate = new Date(created_at);
+		return Math.round(Math.abs((today.getTime() - createdDate.getTime())/ (24*60*60*1000)));
+	}
+
 	createItems = (data) => {
-		let openIssues = data.map((issue, index) => {
+		let openIssues = data.map((issue, index) => {	
+
+			let noOfDays = this.getDateDifference(issue.created_at);
 			return (
-				<div key={index}>
-					
+				<div key={index} className="openIssuesList">
+					<img className="openIssue_img" src="./app/assets/issue-opened.png"/>
+					<p className="openIssue_title">{issue.title}</p>
+					<p className="issue_number">#{issue.number}</p>
+					<p className="issues_created_date">{noOfDays} days ago </p>
+					<p className="issue_opened_by">by {issue.user.login}</p>
 				</div>
 			);
 		});
@@ -22,9 +35,14 @@ export default class RepositoryOpenIssues extends Component {
 	}
 
 	render() {
-		<div>
-			{this.createItems(this.props.data)}
-		</div>
+		return (
+			<div className="openIssues_container">
+				<div className="openIssues_actionBar">
+					<h5>Open issues</h5>
+				</div>
+				{this.createItems(this.props.data)}
+			</div>
+		);
 	}
 
 }
